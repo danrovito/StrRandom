b78b0e11ebcada2c53073e2e3ec7474779bfd9a9
Dan Rovito <danrovito@gmail.com>
2018-02-18 00:28:46 -0500

    random string generation class

---
+++ StrRandom/StrRandom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrRandom
+{
+    public class StrRandom
+    {
+        private readonly Random _random = new Random();
+
+        public const string Alphabet =
+            "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        public string GenerateString(int size = 8)
+        {
+            var chars = new char[size];
+
+            for (var i = 0; i < size; i++)
+            {
+                chars[i] = Alphabet[_random.Next(Alphabet.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+}
